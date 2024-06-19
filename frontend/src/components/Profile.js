14df7a4e1f2849f85309d259f4a6e30a4b302b78
sachin <sachintalreja94@gmail.com>
2024-06-19 13:00:25 +0530

    changes in profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -4,7 +4,7 @@ import { Box,Image,Text,Grid} from '@chakra-ui/react';
 function Profile(){
     return(
         <>
-        <Grid templateColumns='1fr 1fr'>
+        <Grid templateColumns={{base:"1fr" ,md:'1fr 1fr'}}>
          <Box>
             <Text fontSize="xl" fontWeight="semibold"textAlign='center'>Hi,I am Sachin Talreja </Text>
             <Text fontsize="md"  textAlign='center'  padding='5'>Full Stack Developer  who works on both the front-end and back-end of web applications, handling the client-side and server-side development.  proficient in HTML,CSS,JAVASCRIPT frameworks, and databases, ensuring seamless integration and functionality across the entire system. designing and developing scalable software, writing clean code, testing and debugging, and collaborating with cross-functional teams to deliver high-quality solutions</Text>
